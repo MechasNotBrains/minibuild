e51b5ad46b23efba061bc4b5985909aaffed8ae3
sOkam! <heysokam@noreply.codeberg.org>
2026-05-15 13:19:19 +0200

    new: nim: Flags, `minilog` support, and many other refactors

--- src/minibuild.nim
+++ src/minibuild.nim
@@ -1,43 +1,47 @@
 #:_____________________________________________________________
 #  mini.build  |  Copyright (C) Ivan Mar (sOkam!)  |  MPL-2.0 :
 #:_____________________________________________________________
+# @deps std
 from std/os import `/`, splitFile
-from std/strutils import join
+from std/strutils import join, escape
 from std/strformat import `&`
 from std/sequtils import toSeq
+# @deps (Optional)
+when defined(minilog):
+  from minilog as log import nil
 
 
 #_______________________________________
 # @section Type Aliases
 #_____________________________
-type str        * = system.string
 type u8         * = system.uint8
-type Path       * = str
-type SourceList * = system.seq[str]
-type FlagsList  * = system.seq[str]
+type Path       * = system.string
+type SourceList * = system.seq[minibuild.Path]
+type FlagsList  * = system.seq[string]
 
 
 #_______________________________________
 # @section Command Builder
 #_____________________________
+type CommandResult * = object
+  stdin   *:string = ""
+  stdout  *:string = ""
+  code    *:u8     = 0
+#___________________
 type Command * = object
-  parts  :seq[str]= @[]
+  parts   *:seq[string]   = @[]
+  result  *:CommandResult = default(CommandResult)
 #___________________
-func command *(args :varargs[str, `$`]) :Command= Command(parts: args.toSeq())
+func command *(args :varargs[string, `$`]) :Command= Command(parts: args.toSeq())
 #___________________
-func join *(cmd :Command) :str= cmd.parts.join(" ")
+func join *(cmd :Command) :string= cmd.parts.join(" ")
 #___________________
-func add *(cmd :var Command; args :varargs[str, `$`]) :var Command {.discardable.}=
+func add *(cmd :var Command; args :varargs[string, `$`]) :var Command {.discardable.}=
   cmd.parts.add args.toSeq()
   result = cmd
 #___________________
-type CommandResult * = object
-  stdin   :str= ""
-  stdout  :str= ""
-  result  :u8= 0
-#___________________
 proc run *(cmd :Command) :CommandResult {.discardable.}=
-  result = CommandResult(result: os.execShellCmd(cmd.parts.join(" ")).u8)
+  result = CommandResult(code: os.execShellCmd(cmd.parts.join(" ")).u8)
 
 
 #_______________________________________
@@ -46,39 +50,59 @@ proc run *(cmd :Command) :CommandResult {.discardable.}=
 type NimBackend *{.pure.}= enum c, cpp, js
 #___________________
 type ConfigNim * = object
-  bin      :Path= "nim"
-  dir      :Path= ".nim"
-  cache    :Path= ".nim"
-  backend  :NimBackend= NimBackend.c
-#___________________
-type Log *{.pure.}= enum info, quiet, note, verbose, trace
-#___________________
-type LogPrefix * = object
-  tool   :str= "[mini.build]"
-  info   :str= ""
-  note   :str= ""
-  error  :str= " !! ERROR !! "
-  debug  :str= " !! Debug !! "
-  warn   :str= " ! Warning ! "
-#___________________
-type ConfigLog * = object
-  level   :Log= Log.info
-  prefix  :LogPrefix= default(LogPrefix)
-#___________________
-func debug *(L :ConfigLog; args :varargs[str, `$`]) :void=
-  debugEcho &"{L.prefix.tool}{L.prefix.debug}{args.join(\" \")}"
+  bin      *:Path= "nim"
+  dir      *:Path= ".nim"
+  cache    *:Path= ".nim"
+  backend  *:NimBackend= NimBackend.c
 #___________________
 type ConfigDir * = object
-  root  :Path= "."
-  bin   :Path= "bin"
-  src   :Path= "src"
-  lib   :Path= ".lib"
-  sub   :Path= ""
+  root  *:Path= "."
+  bin   *:Path= "bin"
+  src   *:Path= "src"
+  lib   *:Path= ".lib"
+  sub   *:Path= ""
+#___________________
+type ReportMode   *{.pure.}= enum quiet, note, info, verbose, trace
+type ReportTarget *{.pure.}= enum cli, logger, all
+#___________________
+type ReportPrefix * = object
+  tool   *:string= "[mini.build]"
+  info   *:string= ""
+  note   *:string= ""
+  error  *:string= " !! ERROR !! "
+  debug  *:string= " !! Debug !! "
+  warn   *:string= " ! Warning ! "
+#___________________
+type Report * = object
+  level    *:ReportMode   = info
+  prefix   *:ReportPrefix = default(ReportPrefix)
+  mode     *:ReportTarget = all
 #___________________
 type Config * = object
-  dir  :ConfigDir= ConfigDir()
-  nim  :ConfigNim= ConfigNim()
-  log  :ConfigLog= ConfigLog()
+  dir  *:ConfigDir = ConfigDir()
+  nim  *:ConfigNim = ConfigNim()
+  log  *:Report    = Report()
+
+
+#_______________________________________
+# @section Report
+#_____________________________
+func to_cli (R :Report; mode :string; args :varargs[string, `$`]) :void=
+  debugEcho &"{R.prefix.tool}{mode}{args.join(\" \")}"
+#___________________
+func trace *(R :Report; args :varargs[string, `$`]) :void=
+  if R.mode in {all, logger}:
+    when defined(minilog): {.cast(noSideEffect).}: log.trc(args.join(" ").escape) # Logging is always considered safe
+  if R.mode in {all, cli}:
+    if R.level < ReportMode.trace: return
+    R.to_cli(R.prefix.debug, args)
+#___________________
+func debug *(R :Report; args :varargs[string, `$`]) :void=
+  if R.mode in {all, logger}:
+    when defined(minilog): {.cast(noSideEffect).}: log.dbg(args.join(" ").escape) # Logging is always considered safe
+  if R.mode in {all, cli}:
+    if R.level < ReportMode.verbose: return
+    R.to_cli(R.prefix.debug, args)
 
 
 #_______________________________________
@@ -87,6 +111,7 @@ type Config * = object
 type Language *{.pure.}= enum unknown, nim, c, cpp, zig
 #___________________
 func From *(_:typedesc[Language]; src :SourceList) :Language= nim
+#___________________
 func toLanguage *(src :SourceList) :Language= Language.From(src)
 func toLang     *(src :SourceList) :Language= Language.From(src)
 
@@ -98,7 +123,7 @@ type Kind *{.pure.}= enum Program, DynamicLib, StaticLib, UnitTest
 #___________________
 type Target * = object
   src    :SourceList
-  trg    :str       = ""
+  trg    :string    = ""
   cfg    :Config    = default(Config)
   flags  :FlagsList = default(FlagsList)
   lang   :Language  = unknown
@@ -107,12 +132,12 @@ func outDir *(trg :Target) :Path= trg.cfg.dir.root/trg.cfg.dir.bin/trg.cfg.dir.s
 func srcDir *(trg :Target) :Path= trg.cfg.dir.root/trg.cfg.dir.src
 #___________________
 func bin *(trg :Target) :Path= result = case trg.lang
-  of nim : trg.cfg.dir.root/trg.cfg.dir.bin/trg.trg
+  of nim : trg.trg
   else   : trg.trg
 #___________________
 proc entry *(trg :Target) :Path=  trg.src[0]
 #___________________
-func debug *(trg :Target; args :varargs[str, `$`]) :void= trg.cfg.log.debug(args)
+func debug *(trg :Target; args :varargs[string, `$`]) :void= trg.cfg.log.debug(args)
 #___________________
 func target *(
     kind  : Kind;
@@ -120,8 +145,9 @@ func target *(
     trg   : Path       = "";
     src   : SourceList = @[];
     cfg   : Config     = Config();
+    flags : FlagsList  = default(FlagsList)
   ) :Target=
-  result = Target(src: @[], cfg: cfg)
+  result = Target(src: @[], cfg: cfg, flags: flags)
   result.src.add( result.cfg.dir.src/entry )
   for file in src: result.src.add( result.cfg.dir.src/file )
   result.trg = case trg
@@ -133,14 +159,16 @@ func program *(
     trg   : Path       = "";
     src   : SourceList = @[];
     cfg   : Config     = Config();
-  ) :Target= result = minibuild.target(Program, entry, trg, src, cfg)
+    flags : FlagsList  = default(FlagsList)
+  ) :Target= result = minibuild.target(Program, entry, trg, src, cfg, flags)
 #___________________
 proc build *(trg :Target; run :bool= false) :Target {.discardable.}=
   var cmd = minibuild.command(trg.cfg.nim.bin)
   cmd.add($trg.cfg.nim.backend)
   if run: cmd.add("-r")
-  cmd.add(&"--outDir:{trg.outDir()}")
+  for flag in trg.flags: cmd.add(flag)
   cmd.add(&"--out:{trg.bin()}")
+  cmd.add(&"--outDir:{trg.outDir()}")
   cmd.add(trg.entry())
   trg.debug("Build Command:\n  " & cmd.join())
   cmd.run()
@@ -151,7 +179,6 @@ proc build *(trg :Target; run :bool= false) :Target {.discardable.}=
 # @section mini: Entry Point
 #_____________________________
 when isMainModule:
-  echo "Hello mini.build"
   const hello = program("hello.nim")
   hello.build(run=true)
 
