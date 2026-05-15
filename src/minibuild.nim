a9c8ff7a318a34321da1aa2cc4e7a6c033120287
sOkam! <heysokam@noreply.codeberg.org>
2026-05-15 14:31:18 +0200

    new: nim: Minimal Dependencies support

--- src/minibuild.nim
+++ src/minibuild.nim
@@ -117,16 +117,46 @@ func toLang     *(src :SourceList) :Language= Language.From(src)
 
 
 #_______________________________________
+# @section Dependencies
+#_____________________________
+const DependencyDefault_Path   *{.strdefine.}= "src"
+const DependencyDefault_Entry  *{.strdefine.}= ""
+const DependencyDefault_LibDir *{.strdefine.}= ""
+#___________________
+type Dependency * = object
+  name    *:string
+  url     *:string
+  path    *:Path= DependencyDefault_Path
+  entry   *:Path= DependencyDefault_Entry
+  libDir  *:Path= DependencyDefault_LibDir
+  deps    *:seq[Dependency]= @[]
+type Dependencies * = seq[Dependency]
+#___________________
+func dependency *(
+    name  : string;
+    url   : string;
+    path  : Path         = DependencyDefault_Path;
+    entry : Path         = DependencyDefault_Entry;
+    deps  : Dependencies = @[];
+  ) :Dependency= Dependency(name: name, url: url, path: path, entry: entry, deps: deps,)
+#___________________
+func paths *(D :Dependency; cfg :Config) :seq[string]=
+  result.add("--path:" & cfg.dir.bin/cfg.dir.lib/D.name/D.path)
+  for dep in D.deps: result.add dep.paths(cfg)
+
+
+#_______________________________________
 # @section Target
 #_____________________________
 type Kind *{.pure.}= enum Program, DynamicLib, StaticLib, UnitTest
 #___________________
 type Target * = object
   src    :SourceList
-  trg    :string    = ""
-  cfg    :Config    = default(Config)
-  flags  :FlagsList = default(FlagsList)
-  lang   :Language  = unknown
+  trg    :string       = ""
+  lang   :Language     = unknown
+  cfg    :Config       = default(Config)
+  flags  :FlagsList    = default(FlagsList)
+  deps   :Dependencies = @[]
 #___________________
 func outDir *(trg :Target) :Path= trg.cfg.dir.root/trg.cfg.dir.bin/trg.cfg.dir.sub
 func srcDir *(trg :Target) :Path= trg.cfg.dir.root/trg.cfg.dir.src
@@ -142,12 +172,13 @@ func debug *(trg :Target; args :varargs[string, `$`]) :void= trg.cfg.log.debug(a
 func target *(
     kind  : Kind;
     entry : Path;
-    trg   : Path       = "";
-    src   : SourceList = @[];
-    cfg   : Config     = Config();
-    flags : FlagsList  = default(FlagsList)
+    trg   : Path         = "";
+    src   : SourceList   = @[];
+    cfg   : Config       = Config();
+    flags : FlagsList    = default(FlagsList);
+    deps  : Dependencies = @[];
   ) :Target=
-  result = Target(src: @[], cfg: cfg, flags: flags)
+  result = Target(src: @[], cfg: cfg, flags: flags, deps: deps)
   result.src.add( result.cfg.dir.src/entry )
   for file in src: result.src.add( result.cfg.dir.src/file )
   result.trg = case trg
@@ -156,17 +187,19 @@ func target *(
 #___________________
 func program *(
     entry : Path;
-    trg   : Path       = "";
-    src   : SourceList = @[];
-    cfg   : Config     = Config();
-    flags : FlagsList  = default(FlagsList)
-  ) :Target= result = minibuild.target(Program, entry, trg, src, cfg, flags)
+    trg   : Path         = "";
+    src   : SourceList   = @[];
+    cfg   : Config       = Config();
+    flags : FlagsList    = default(FlagsList);
+    deps  : Dependencies = @[];
+  ) :Target= result = minibuild.target(Program, entry, trg, src, cfg, flags, deps)
 #___________________
 proc build *(trg :Target; run :bool= false) :Target {.discardable.}=
   var cmd = minibuild.command(trg.cfg.nim.bin)
   cmd.add($trg.cfg.nim.backend)
   if run: cmd.add("-r")
   for flag in trg.flags: cmd.add(flag)
+  for dep  in trg.deps:  cmd.add(dep.paths(trg.cfg))
   cmd.add(&"--out:{trg.bin()}")
   cmd.add(&"--outDir:{trg.outDir()}")
   cmd.add(trg.entry())
